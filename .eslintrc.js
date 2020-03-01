8ce0952db6d3be6c94efbd72288dc30edda99c55
shuufei <digital.shuufei@gmail.com>
2020-03-01 20:15:46 +0900

    eslintとprettierを導入

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'airbnb-typescript',
+    // 'prettier/@typescript-eslint',
+  ],
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  rules: {
+    // 'prettier/prettier': 'error',
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    indent: ['error', 2],
+    'no-console': ['error', { allow: ['warn', 'error', 'log'] }]
+  }
+};
