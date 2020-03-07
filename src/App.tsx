9c261fc7d2c39b74ec0642ef2d8a97d10ab271bc
shuufei <digital.shuufei@gmail.com>
2020-03-07 12:39:17 +0900

    ドラッグアンドドロップのテスト実装

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,30 @@ import { jsx, css } from '@emotion/core';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => (
-  <div className="App">
-    <TextInputWithFocusButton />
-  </div>
-);
+const App: React.FC = () => {
+  document.addEventListener('dragenter', function(e) {
+    console.log(
+      '--- dragenter: ',
+      (e.target as any).className.indexOf('dropzone')
+    );
+    if ((e.target as any).className.indexOf('dropzone') >= 0) {
+      (e.target as any).style.background = 'blue';
+    }
+    if ((e.target as any).className.indexOf('parent') >= 0) {
+      (e.target as any).style.background = 'red';
+    }
+  });
+  return (
+    <div className="App">
+      <TextInputWithFocusButton />
+      <DraggableContent uuid={'0'} />
+      <DropZone />
+      <DraggableContent uuid={'1'} />
+      <DropZone />
+      <DraggableContent uuid={'2'} />
+    </div>
+  );
+};
 
 export default App;
 
@@ -43,5 +62,55 @@ type TextInputPros = {
 };
 
 export const TextInput: React.FC<TextInputPros> = ({ inputRef }) => {
-  return <input type="text" ref={inputRef} />;
+  const keyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    console.log('--- event: ', e.keyCode, e.key, e);
+  };
+  return <input type="text" ref={inputRef} onKeyDown={e => keyPress(e)} />;
 };
+
+export const DraggableContent: React.FC<{ uuid: string }> = ({ uuid }) => {
+  const [draggable, setDraggable] = useState(false);
+  const dragstart = (event: React.DragEvent<HTMLDivElement>) => {
+    event.dataTransfer.setData('text/plain', uuid);
+    event.dataTransfer.effectAllowed = 'move';
+  };
+  return (
+    <div
+      css={css`
+        background: #a0a0a0;
+        padding: 8px 12px;
+        margin: 12px;
+        width: 200px;
+        height: 40px;
+        box-sizing: border-box;
+      `}
+      id={uuid}
+      className="parent"
+      draggable={draggable}
+      onDragEnd={() => setDraggable(false)}
+    >
+      <div
+        css={css`
+          width: 20px;
+          height: 20px;
+          background-color: #000;
+        `}
+        onMouseDown={() => setDraggable(true)}
+        onMouseUp={() => setDraggable(false)}
+        onDragStart={event => dragstart(event)}
+      ></div>
+    </div>
+  );
+};
+
+export const DropZone: React.FC = () => (
+  <div
+    css={css`
+      width: 200px;
+      height: 5px;
+      background: #ccc;
+      margin: 8px 12px;
+    `}
+    className="dropzone"
+  ></div>
+);
